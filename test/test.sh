de424b113a9701c6055c73710c444cb3d3564440
Jaret Cantu <jaret.cantu@timesys.com>
2015-06-14 01:09:36 -0400

    Add a test interface.
    This is a simple sample Kconfig/.config generating interface to
    generate and test the values from the new arithmetic comparison
    operations.

---
+++ test/test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+WD=$(dirname `readlink -f $0`)
+BUILDDIR=$WD/..
+
+export KBUILD_KCONFIG=$WD/Kconfig
+
+# Generate test configuration
+$WD/gen_kconfig < $WD/testlist > $KBUILD_KCONFIG
+make -C $BUILDDIR oldconfig
+
+$WD/check_kconfig < $BUILDDIR/.config
